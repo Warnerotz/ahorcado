4ea34306257edbabb0aa08158969f715b9837355
Warnerotz <32178238+Warnerotz@users.noreply.github.com>
2017-09-25 13:32:18 +0200

    Add files via upload

--- js/codigo.js
+++ js/codigo.js
@@ -1,11 +1,11 @@
-window.onload = function () {    
-    var arrayAhorcado= ["A","H","O","R","C","A","D","O"];
+window.onload = function () {
+    var arrayAhorcado = ["A", "H", "O", "R", "C", "A", "D", "O"];
     var adivina = seleccionarPalabra();
     var adivinaCadena = adivina.split("");
     var numIntentos = arrayAhorcado.length;
-    var numAciertos = 0;    
-    //muestra las instrucciones
-    document.getElementById("instrucciones").addEventListener("mouseover", mostrar, false);
+    var numAciertos = 0;
+   
+   
     //muestra la pantalla de juego
     document.getElementById("jugar").addEventListener("click", mostrar, false);
     //crea los inputs de la palabra a adivinar
@@ -30,14 +30,14 @@ window.onload = function () {
                 document.getElementById("celdaletra" + i).value = this.value;
                 numAciertos++;
             }
-            
+
             //cuando acierte la palabra saldra un mensaje que ha acertado la palabra
-            
-            if(numAciertos == adivinaCadena.length){
-                
+
+            if (numAciertos == adivinaCadena.length) {
+
                 alert("palabra acertada");
             }
-            
+
         }
         //si la letra es correcta se pone verde y se bloquea sino roja y se bloquea
         if (encontrada == true) {
@@ -46,13 +46,13 @@ window.onload = function () {
         } else {
             this.classList.add('btn-danger');
             this.setAttribute("disabled", "disabled");
-            
-            document.getElementById("celdaAhor"+(arrayAhorcado.length-numIntentos)).style.display= "inline";
-            document.getElementById("celdaAhor"+(arrayAhorcado.length-numIntentos)).style.backgroundColor= "transparent";
+
+            document.getElementById("celdaAhor" + (arrayAhorcado.length - numIntentos)).style.display = "inline";
+            document.getElementById("celdaAhor" + (arrayAhorcado.length - numIntentos)).style.backgroundColor = "transparent";
             numIntentos--;
-            
-            if(numIntentos==0){
-                
+
+            if (numIntentos == 0) {
+
                 alert("Has perdido tio");
             }
         }
@@ -74,17 +74,13 @@ window.onload = function () {
     }
 
     function mostrar() {
+       
         //segun el id que le entra hace unas cosas u otras.
         switch (this.id) {
-            //en caso de que le des a instrucciones muestra el cuadro de instrucciones.
-            case "instrucciones":
-                document.getElementById("ins").style.display = "block";
-                break;
-                //en caso de que le des a jugar ocultara toda la pantalla y mostrara la pantalla de juego             
             case "jugar":
                 //lo que oculta.
-                document.getElementById("botonera").style.display = "none";
-                document.getElementById("ins").style.display = "none";
+                
+                document.getElementById("inicio").style.display = "none";                
                 //muestra el juego.
                 document.getElementById("juego").style.display = "block";
                 break;
@@ -100,36 +96,41 @@ window.onload = function () {
         var textoAdivinar = document.getElementById("textoAdivinar");
 
         for (var i = 0; i < adivina.length; i++) {
+            var div= document.createElement("div");
+            div.classList.add("col-md-1");
             var input = document.createElement("input");
             input.setAttribute("type", "text");
             input.setAttribute("style", "background-color:transparent");
             input.setAttribute("maxlength", "1");
+            input.setAttribute("readonly","readonly");
             input.setAttribute("class", "claseAdivina");
             input.setAttribute("id", "celdaletra" + i);
             input.setAttribute("value", "_");
-            textoAdivinar.appendChild(input);
+            div.appendChild(input);
+            textoAdivinar.appendChild(div);
         }
-        
+
         //crea los espacios de la palabra ahorcado
         crearAhorcado();
-
+asd
     }
 
     function crearAhorcado() {
-        
-        
+
+
         for (var i = 0; i < arrayAhorcado.length; i++) {
             var input = document.createElement("input");
             input.setAttribute("type", "text");
             input.setAttribute("style", "background-color:transparent");
             input.setAttribute("maxlength", "1");
+            input.setAttribute("readonly","readonly");
             input.setAttribute("class", "claeAhorcado");
             input.setAttribute("id", "celdaAhor" + i);
             input.setAttribute("value", arrayAhorcado[i]);
-            input.setAttribute("style","display:none");
+            input.setAttribute("style", "display:none");
             document.getElementById("textoAhorcado").appendChild(input);
 
-        }   
+        }
 
 
     }
