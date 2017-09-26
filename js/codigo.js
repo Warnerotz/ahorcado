8a890a8c9afad53a1276c1f17c8dc49826b48fc4
warnerotz <adrichony@gmail.com>
2017-09-26 15:00:19 +0200

    26/09/2017 15:00

--- js/codigo.js
+++ js/codigo.js
@@ -1,21 +1,55 @@
 window.onload = function () {
     var arrayAhorcado = ["A", "H", "O", "R", "C", "A", "D", "O"];
-    var abecedario = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"];
+    var abecedario = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var adivina = seleccionarPalabra();
     var adivinaCadena = adivina.split("");
     var numIntentos = arrayAhorcado.length;
     var numAciertos = 0;
-   
-   
+    crearTeclado();
+    crearTablaAhorcado();
+
     //muestra la pantalla de juego
     document.getElementById("jugar").addEventListener("click", mostrar, false);
     //crea los inputs de la palabra a adivinar
-    document.getElementById("jugar").addEventListener("click", crearTablaPalabra, false);
+    //document.getElementById("jugar").addEventListener("click", crearTablaPalabra, false);
+    document.getElementById("reiniciar").addEventListener("click", reiniciar, false);
     //comprobar botones
     var teclado = document.getElementsByClassName("botonTeclado");
-    for (var i = 0; i < teclado.length; i++) {
+    listenersTeclado();
+
+    function reiniciar() {
+        
+        var tAdivine = document.getElementById("tablaPalabra");
+        tAdivine.parentNode.removeChild(tAdivine);
+        var tAhor = document.getElementById("tablaAhorcado");
+        tAhor.parentNode.removeChild(tAhor);
+        var tTeclado = document.getElementById("tablaBotones");
+        tTeclado.parentNode.removeChild(tTeclado);
+        var adivina = seleccionarPalabra();
+        var adivinaCadena = adivina.split("");
+        var numIntentos = arrayAhorcado.length;
+        var numAciertos = 0;
+        crearTeclado();
+        var teclado = document.getElementsByClassName("botonTeclado");
+        listenersTeclado();
+        crearTablaPalabra();
+        crearTablaAhorcado();
+    }
+
+    function listenersTeclado(){
+        for (var i = 0; i < teclado.length; i++) {
 
         teclado[i].addEventListener("click", comprueba, false);
+    }    
+        
+    }
+    
+    
+    function salir() {
+        location.reload();
+
+
+
     }
 
     function comprueba() {
@@ -35,8 +69,9 @@ window.onload = function () {
             //cuando acierte la palabra saldra un mensaje que ha acertado la palabra
 
             if (numAciertos == adivinaCadena.length) {
+                document.getElementById("gana").style.display = "flex";
+                document.getElementById("botonReiniciar").style.display = "flex"; //document.getElementById("teclado").style.display = "none";
 
-                alert("palabra acertada");
             }
 
         }
@@ -68,6 +103,7 @@ window.onload = function () {
 
     }
 
+
     function seleccionarPalabra() {
         var listaPalabras = ["coche", "moto", "camion"];
         var random = Math.floor(Math.random() * listaPalabras.length);
@@ -75,41 +111,48 @@ window.onload = function () {
     }
 
     function mostrar() {
-       
+
         //segun el id que le entra hace unas cosas u otras.
         switch (this.id) {
             case "jugar":
-                //lo que oculta.
-                
-                document.getElementById("inicio").style.display = "none";                
+                //lo que oculta.                
+                document.getElementById("inicio").style.display = "none";
                 //muestra el juego.
                 document.getElementById("juego").style.display = "block";
                 break;
 
-
         }
+
+        crearTablaPalabra();
 
 
     }
     //funcion para crear el espacio de la palabra a adivinar 
-    function crearTablaPalabra(){
+    function crearTablaPalabra() {
+        var div = document.createElement("div");
+        div.setAttribute("id","tablaPalabra");
+        div.classList.add("col-md-6");
+        div.classList.add("centrarTabla");
         var tbl1 = document.createElement('table');
         tbl1.classList.add("tabla");
-        tbl1.classList.add("col-md-6");
+        tbl1.setAttribute("id", "pAdivina");
         var tr1 = tbl1.insertRow();
-        for(var i = 0; i < adivina.length; i++)  {
+        for (var i = 0; i < adivina.length; i++) {
             var td1 = tr1.insertCell();
-            td1.setAttribute("id", "celdaLetra"+i);
+            td1.setAttribute("id", "celdaLetra" + i);
             td1.setAttribute("class", "claseAdivina");
             td1.appendChild(document.createTextNode(""));
             td1.style.borderBottom = "thick solid black";
-            td1.style.width="40px";;
-        }   
-        document.getElementById("textoAdivinar").appendChild(tbl1);
-        
-        crearTablaAhorcado(); 
+            td1.style.width = "40px";;
+        }
+        document.getElementById("textoAdivinar").appendChild(div);
+        div.appendChild(tbl1);
+
+
+
+
     }
-  /*  
+    /*  
     function crearPalabra() {
 
         var textoAdivinar = document.getElementById("textoAdivinar");
@@ -134,25 +177,30 @@ window.onload = function () {
         crearAhorcado();
 
     }
-*/    
-    
-    function crearTablaAhorcado(){
-        
-        
+*/
+
+    function crearTablaAhorcado() {
+
+
         // creamos la tabla con la palabra AHORCADO
+        var div = document.createElement("div");
+        div.setAttribute("id","tablaAhorcado");
+        div.classList.add("col-md-6");
+        div.classList.add("centrarTabla");
         var tbl1 = document.createElement('table');
         tbl1.classList.add("tabla");
-        tbl1.classList.add("col-md-6");
+        tbl1.setAttribute("id", "pAhorcado");
         var tr1 = tbl1.insertRow();
-        for(var i = 0; i < arrayAhorcado.length; i++)  {
+        for (var i = 0; i < arrayAhorcado.length; i++) {
             var td1 = tr1.insertCell();
-            td1.setAttribute("id", "celdaAhor"+i);
+            td1.setAttribute("id", "celdaAhor" + i);
             td1.setAttribute("class", "claeAhorcado");
             td1.appendChild(document.createTextNode(arrayAhorcado[i]));
             td1.style.borderBottom = "thick solid #F5F5DC";
             td1.style.display = "none";
-        }   
-        document.getElementById("textoAhorcado").appendChild(tbl1);
+        }
+        document.getElementById("textoAhorcado").appendChild(div);
+        div.appendChild(tbl1);
     }
 
     /*
@@ -177,17 +225,33 @@ window.onload = function () {
 
     }
     */
-    
 
-    function crearTeclado(){
-        
+
+    function crearTeclado() {
+        var z = 0;
         var tabla = document.createElement('table');
-        for(var i =0; i<3; i++){
-            var fila = tabla.insertRow();
-            
-            
+        tabla.classList.add("col-lg-6");
+        tabla.setAttribute("id", "tablaBotones");
+        for (var i = 0; i < 3; i++) {
+            var fila = document.createElement('tr');
+            for (var j = 0; j < 9; j++) {
+                var td = document.createElement('td');
+                //creo el imput
+                var input = document.createElement('input');
+                input.setAttribute("type", "button");
+                input.classList.add("btn");
+                input.classList.add("btn-default");
+                input.classList.add("botonTeclado");
+                input.setAttribute("value", abecedario[z]);
+                td.appendChild(input);
+                fila.appendChild(td);
+                z++;
+            }
+            tabla.appendChild(fila);
+
         }
-        
+        document.getElementById("teclado").appendChild(tabla)
+
     }
 
 
